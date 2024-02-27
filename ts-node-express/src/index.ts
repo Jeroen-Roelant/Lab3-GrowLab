839ecfa7cb1d33ee41b8069a52da63605a86787c
Jeroen Roelant <jeroenroelant@hotmail.com>
2024-02-27 16:27:38 +0100

    test API works

--- ts-node-express/src/index.ts
+++ ts-node-express/src/index.ts
@@ -1,11 +1,20 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import * as bodyParser from 'body-parser';
+import routes from './routes/route';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// applying the routes to the basepath '/api'
+app.use('/api', routes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express & TypeScript Server running");
