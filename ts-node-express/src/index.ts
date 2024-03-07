33f511bbbf5df9efe4d244c9d84741d2ee585cee
Jeroen Roelant <s121087@ap.be>
2024-03-07 16:44:04 +0100

    New informative index and cli
    new package: momentjs
    
    npm i moment --save

--- ts-node-express/src/index.ts
+++ ts-node-express/src/index.ts
@@ -6,12 +6,13 @@ import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes/route';
 import { dbTest } from "./config/db";
+import moment from "moment";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
+const startTime: Date = new Date();
 
 // body parser middleware
 app.use(bodyParser.json());
@@ -26,9 +27,17 @@ app.use('/api', routes);
 app.get("/", async (req: Request, res: Response) => {
   let dbState: string = "MySQL Connection failed";
   if (await dbTest()){
-    dbState = "MySQL connection made ";
+    dbState = "MySQL Connection made";
   }
-  let msg: string = `Express & TypeScript API Server running, ${dbState}`;
+  
+  let msg: string = 
+  `<pre>
+  🌿 <b>GrowLab</b> by Jeroen Roelant & Marlène Braem. <br>
+  🌐 Express 4.18.2 & TypeScript API Server running. <br>
+  ⌚ Uptime ${moment.utc(moment().diff(moment(startTime))).format('HH:mm:ss')}.<br>
+  🪣 ${dbState}. <br>
+  🗺️ ${routes.stack.length} Available routes. <br>
+  </pre>`;
 
   res.send(msg);
 });
@@ -36,9 +45,9 @@ app.get("/", async (req: Request, res: Response) => {
 app.listen(port, async () => {
   console.log(  `🟢 [Express]:  Server is running at http://localhost:${port} `);
   if(await dbTest()){
-    console.log(`🟢 [MySQL]:    MySQL database Connection made at http://${process.env.DB_HOST}/${process.env.DB_PORT} on DB ${process.env.DB_DATABASE} user \'${process.env.DB_USER}\'`);
+    console.log(`🟢 [MySQL]:    MySQL database Connection made at http://${process.env.DB_HOST}/${process.env.DB_PORT} using user \'${process.env.DB_USER}\'`);
   }
   else{
-    console.log(`🔴 [MySQL]:    MySQL database failed to connect at http://${process.env.DB_HOST}:${process.env.DB_PORT} on DB ${process.env.DB_DATABASE} user \'${process.env.DB_USER}\'`);
+    console.log(`🔴 [MySQL]:    MySQL database failed to connect at http://${process.env.DB_HOST}:${process.env.DB_PORT} using user \'${process.env.DB_USER}\'`);
   }
 });
