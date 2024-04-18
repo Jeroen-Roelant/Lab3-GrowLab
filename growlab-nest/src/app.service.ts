158559bc8360a741bab43357dc69ab789ec153eb
Jeroen Roelant <s121087@ap.be>
2024-04-18 12:24:19 +0200

    Structured hello better

--- growlab-nest/src/app.service.ts
+++ growlab-nest/src/app.service.ts
@@ -1,39 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    
-    let msg: string = 
-  `
-<!DOCTYPE html>
-<html>
-  <pre>
-    <head>
-      <title> 🌿 GrowLab NestJS running</title>
-    </head> 
-    <body>
-      <div>
-        <p>
-🌿 <b>GrowLab</b> by <a href="https://github.com/Jeroen-Roelant">Jeroen Roelant</a> & <a href="https://github.com/marlenebraem1999">Marlène Braem.</a> <br>
-🌐 NEST & TypeScript API Server running. <br>
-📦 MySQL Connection made. <br>
-        </p>
-      </div>
-    </body>
-  </pre>
-  <style>
-    body{
-      height: 100vh;
-      margin: 0px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  </style>
-<html>
-  `;
+    const filePath = join(__dirname, '../hello.html');
+    const htmlContent = readFileSync(filePath, 'utf8');
 
-    return msg;
+    return htmlContent;
   }
 }
